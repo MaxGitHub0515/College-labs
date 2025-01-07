757069fc31e87bf65fded3702a1e5d4e844694d4
MaxGitHub0515 <pavlovskymaxym4@gmail.com>
2025-01-07 10:53:30 +0100

    uploaded

---
+++ lab11/Student.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+#include<string>
+using namespace std;
+// store info about 256 students and change it later
+const int MAX = 256; // maks liczba wpisow/rekordow w bazie
+class Student {
+    public:
+    string imie;
+    string adres;
+    string tel;
+};
