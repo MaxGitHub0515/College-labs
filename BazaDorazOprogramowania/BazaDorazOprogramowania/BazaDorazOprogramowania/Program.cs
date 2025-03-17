7146ae533066ad4c303def2d100c2565d90e3626
MaxGitHub0515 <105605213+MaxGitHub0515@users.noreply.github.com>
2025-03-17 13:48:31 +0100

    uploaded

---
+++ BazaDorazOprogramowania/BazaDorazOprogramowania/BazaDorazOprogramowania/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.EntityFrameworkCore.Query;
+using 
+
+
+
+/* DB CONNECTION STRING */
+//"Server = 10.8.0.16; Database = maxpavlovskyim; User Id = BAZAORAZOPROGRAMOWANIE; Password = 40566; MultipleActiveResultSets = true"
+
