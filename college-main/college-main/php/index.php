9fe5d1235738c6dfa1ac3903eb4645e60bc606e0
MaxGitHub0515 <pavlovskymaxym4@gmail.com>
2025-03-31 16:31:15 +0200

    updated

---
+++ college-main/college-main/php/index.php
@@ -0,0 +1,4 @@
+
+<!-- https://www.apachefriends.org/pl/download.html -->
+
+<!-- https://www.jetbrains.com/phpstorm/promo/?source=google&medium=cpc&campaign=EMEA_en_PL_PhpStorm_Branded&term=phpstorm&content=604147130393&gad_source=1&gclid=CjwKCAjw-qi_BhBxEiwAkxvbkNg6cK-f0vrKIZLrlepFALJQv7ZFTf2QNB_wrrDwJaiOA-Oi-lY_-RoC_u4QAvD_BwE -->
