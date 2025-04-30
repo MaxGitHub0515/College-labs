a3b3a1ad48394776939ab4c637050638067c8303
MaxGitHub0515 <105605213+MaxGitHub0515@users.noreply.github.com>
2025-04-30 08:13:03 +0200

    Update Frame.java

--- college-main/college-main/java/cwiczenie5_task5/src/myapp/Frame.java
+++ college-main/college-main/java/cwiczenie5_task5/src/myapp/Frame.java
@@ -1,27 +1,30 @@
 package myapp;
 
-import java.awt.Button;
-import java.awt.Frame;
-import java.awt.HeadlessException;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
+// import java.awt.Button;
+// import java.awt.Frame;
+// import java.awt.HeadlessException;
+// import java.awt.event.ActionEvent;
+// import java.awt.event.ActionListener;
+// import java.awt.event.WindowEvent;
+// import java.awt.event.WindowListener;
 
-// cwiczenie 4 - nie skonczone
+// // cwiczenie 4 - nie skonczone
 
-public class OknoZPrzyciskami extends Frame implements WindowListener, ActionListener {
-    private Button przycisk1, przycisk2, przycisk3, przycisk4, przycisk5;
+// public class OknoZPrzyciskami extends Frame implements WindowListener, ActionListener {
+//     private Button przycisk1, przycisk2, przycisk3, przycisk4, przycisk5;
 
-    public OknoZPrzyciskami() throws HeadlessException {
-        super("Okno z wieloma przyciskami");
-        setBounds(10, 10, 500, 400);
-        setLayout(null);
+//     public OknoZPrzyciskami() throws HeadlessException {
+//         super("Okno z wieloma przyciskami");
+//         setBounds(10, 10, 500, 400);
+//         setLayout(null);
  
-    }
+//     }
 
-    public static void main(String[] args) {
-        new OknoZPrzyciskami();
-    }
+//     public static void main(String[] args) {
+//         new OknoZPrzyciskami();
+//     }
+
+
+
 
    
